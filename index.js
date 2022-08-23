08c4a0611f26621910943b19e356749e50dfde98
Maureen Murugi <maureen.murugi@student.moringaschool.com>
2022-08-23 08:14:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = [
+    'Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+  function findMatching(newArray, string) {
+       return newArray.filter(
+           (matchingName) => matchingName.toUpperCase() === string.toUpperCase()
+       );
+   } 
+
+   function fuzzyMatch(newArray, newString) {
+       return newArray.filter(
+           (matchingName) => matchingName.toUpperCase().indexOf(newString.toUpperCase()) === 0
+       );
+   }
+
